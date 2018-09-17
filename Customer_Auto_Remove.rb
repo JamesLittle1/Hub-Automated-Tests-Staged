05b9b19824a9165efecd2d11f02836a0e037aab5
James Little <James.Little@makeitcheaper.com>
2018-09-17 13:21:56 +0100

    Fixing Customer_Auto_Remove

--- Customer_Auto_Remove.rb
+++ Customer_Auto_Remove.rb
@@ -1,7 +1,7 @@
 require 'tiny_tds'
 
 client = TinyTds::Client.new(:dataserver => ENV['DATASERVER_STAGED'], :database => ENV['DATABASE_STAGED'], :username => ENV['USERNAME_SQL'], :password => ENV['PASSWORD_SQL'])
-result = client.execute('SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON 
-SET NUMERIC_ROUNDABORT OFF
-exec [customer].[CustomerAutoRemove]')
+sql = 'SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON; SET NUMERIC_ROUNDABORT OFF; exec [customer].[CustomerAutoRemove];'
+result = client.execute(sql)
+result.do
 puts "customer should now be deleted"
