fc20259119bb3e8e987fd0d86e8aa5d85427d1fb
James Little <James.Little@makeitcheaper.com>
2018-09-19 09:58:00 +0100

    Moving Check_Customer_Has_Email into frame

--- Check_Customer_Has_Email.rb
+++ Check_Customer_Has_Email.rb
@@ -8,18 +8,12 @@ config = YAML.load_file("./config.yml")
 
 session = Capybara::Session.new :selenium_firefox
 
-
-if(!login(session, config, ARGV[0], ARGV[1]))
-	puts "Failed to log onto Hub"
+if(!open_search_customer_frame(session, config, ARGV[0], ARGV[1]))
+	puts "Open_search_customer_frame failed"
 	exit -1
 end
 
-if(!search_customers(session, config, ARGV[0], ARGV[1]))
-	puts "Search_customers failed"
-	exit -1
-end
-
-if(!select_customer(session, config, true))
+if(!select_customer(session, config, true, true))
 	puts "Search_customers failed"
 	exit -1
 end
@@ -30,9 +24,7 @@ def check_customer_has_email(session, config)
 		return false
 	end
 	if(no_email[0])
-		session.within_frame(0) do
-			session.find(:id, "ctl00_MainArea_ProspectPageMailInfo1_imgContactEdit").click
-		end
+		session.find(:id, "ctl00_MainArea_ProspectPageMailInfo1_imgContactEdit").click
 		return fill_in_email(session, config)
 	else
 		puts "Customer already has email!"
@@ -42,23 +34,19 @@ end
 
 def fill_in_email(session, config)
 	if(!wait_for_page_to_load(session, config, 'loop_times', 'timeout_threshold', "Edit Contact Information", "load"){
-		session.within_frame(0) do
-			begin
-				session.click_button("ctl00_MainArea_ucMntCustomerContact_btnAddEmails")
-			rescue Capybara::ElementNotFound => e
-				session.find(:xpath, "/html/body/form/div[5]/div/div[2]/div/div[2]/div[1]/div[3]/div[2]/div[1]").click
-				session.click_button("ctl00_MainArea_ucMntCustomerContact_btnAddEmails")
-			end
-			session.find(:id, "ctl00_MainArea_ucMntCustomerContact_rptEmails_ctl01_ucMntEmail_txtEmailAddress_text").send_keys(config['email'])
-			session.click_button("ctl00_MainArea_ucMntCustomerContact_btnSave")
+		begin
+			session.click_button("ctl00_MainArea_ucMntCustomerContact_btnAddEmails")
+		rescue Capybara::ElementNotFound => e
+			session.find(:xpath, "/html/body/form/div[5]/div/div[2]/div/div[2]/div[1]/div[3]/div[2]/div[1]").click
+			session.click_button("ctl00_MainArea_ucMntCustomerContact_btnAddEmails")
 		end
+		session.find(:id, "ctl00_MainArea_ucMntCustomerContact_rptEmails_ctl01_ucMntEmail_txtEmailAddress_text").send_keys(config['email'])
+		session.click_button("ctl00_MainArea_ucMntCustomerContact_btnSave")
 	})
 		return false
 	end
 	if(!wait_for_page_to_load(session, config, 'loop_times', 'timeout_threshold', "Email", "save"){
-		session.within_frame(0) do
-			session.click_button("ctl00_MainArea_ucMntCustomerContact_btnClose")
-		end
+		session.click_button("ctl00_MainArea_ucMntCustomerContact_btnClose")
 	})
 		return false
 	end
@@ -77,16 +65,14 @@ end
 
 def check(session, config, no_email)
 	if(!wait_for_page_to_load(session, config, 'loop_times', 'timeout_threshold', "check_customer_has_email", "run"){
-		session.within_frame(0) do
-			session.find(:id, "ctl00_MainArea_ProspectPageMailInfo1_cmbEmail_Input").click
-			if(session.find(:id, "ctl00_MainArea_ProspectPageMailInfo1_cmbEmail_DropDown")['innerHTML'].scan(config['email']).count == 0)
-				no_email[0] = true
-			end
+		session.find(:id, "ctl00_MainArea_ProspectPageMailInfo1_cmbEmail_Input").click
+		if(session.find(:id, "ctl00_MainArea_ProspectPageMailInfo1_cmbEmail_DropDown")['innerHTML'].scan(config['email']).count == 0)
+			no_email[0] = true
 		end
-		return true
 	})
 		return false
 	end
+	return true
 end
 
 if(!check_customer_has_email(session, config))
