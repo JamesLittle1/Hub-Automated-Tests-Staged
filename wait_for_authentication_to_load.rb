eb6ba6b441f0544c87564d5f86b9b6450e507e45
James Little <James.Little@makeitcheaper.com>
2018-09-25 08:40:02 +0100

    Replacing block.call with the faster yield method

--- wait_for_authentication_to_load.rb
+++ wait_for_authentication_to_load.rb
@@ -1,9 +1,9 @@
-def wait_for_authentication_to_load(session, config, loop_times, timeout_threshold, noun="", verb="", &block)
+def wait_for_authentication_to_load(session, config, loop_times, timeout_threshold, noun="", verb="")
 	load_attempts = 0
 	for i in 0..config[loop_times]
 		sleep(config[timeout_threshold])
 		begin
-			block.call
+			yield
 			if(noun != "" || verb != "")
 				puts "Waited #{i + 1} second(s) for " + noun + " to " + verb + "."
 			end
--- wait_for_page_to_load.rb
+++ wait_for_page_to_load.rb
@@ -1,9 +1,9 @@
-def wait_for_page_to_load(session, config, loop_times, timeout_threshold, noun="", verb="", &block)
+def wait_for_page_to_load(session, config, loop_times, timeout_threshold, noun="", verb="")
 	load_attempts = 0
 	for i in 0..config[loop_times]
 		sleep(config[timeout_threshold])
 		begin
-			block.call # This is where the code is called
+			yield # This is where the code is called
 			if(noun != "" || verb != "")
 				puts "Waited #{i + 1} second(s) for " + noun + " to " + verb + "."
 			end
